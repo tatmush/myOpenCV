70a8b1eeff4fa16ef129c3b2ed7bd4829a6b848a
tatmush <tatalmondmush@gmail.com>
2018-07-12 22:10:07 +0200

    crop the object that you have detected

--- objectDetection1.py
+++ objectDetection1.py
@@ -1,12 +1,12 @@
 import cv2
 
+#detect edges
 def detectImage(image):
     #variable for measuring the speed of the function
     e1=cv2.getTickCount()
-    #reading the image
-    img=cv2.imread(image)
+    
     #canny library is used to identify edges in an picture
-    edges=cv2.Canny(img, 200,300)
+    edges=cv2.Canny(img, 100,300)
     
     #display using mapplotlib
     '''plt.subplot(121), plt.imshow(img, cmap='gray')
@@ -27,4 +27,24 @@ def detectImage(image):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-detectImage('object4.png')
+    return edges
+
+#crop image
+def cropImage(OriginalImage, edgedImaged):
+    #find all the edges in the image and trace them objects
+    (contours, _)=cv2.findContours(edgedImaged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    index=0
+    for contour in contours:
+        x,y,w,h=cv2.boundingRect(contour)
+        
+        #determine w and h depending on the type of object you want to identify
+        if 75 and h>75:
+            index+=1
+            croppedImg=OriginalImage[y:y+h, x:x+w]
+            cv2.imwrite(str(index)+'.png', croppedImg)
+
+image='object4.png'
+#reading the image
+img=cv2.imread(image)
+edgedImage=detectImage(img)
+cropImage(img, edgedImage)
